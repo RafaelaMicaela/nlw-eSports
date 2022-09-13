904d8087871f67deaf404818af92c7ffcb37344f
rafaelamicaela <rafaela.micaela@academico.ifrn.edu.br>
2022-09-13 17:06:04 -0300

    first commit

---
+++ mobile/App.tsx
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      
+      <Button title="Send 1"/>
+      <Button title="Send 2"/>
+      <Button title="Send 3"/>
+      <Button title="Hello World"/>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+interface ButtonProps{
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return(
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});

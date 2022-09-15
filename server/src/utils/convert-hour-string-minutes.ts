4934e2fc6d3b7f3a9bcc9fc8774d7aa34d3081c6
rafaelamicaela <rafaela.micaela@academico.ifrn.edu.br>
2022-09-15 19:13:33 -0300

    config back and database

---
+++ server/src/utils/convert-hour-string-minutes.ts
@@ -0,0 +1,10 @@
+// 10:00 -> 1000
+
+export function convertHourStringToMinutes(hourString: string){
+    const [hours, minutes] = hourString.split(':').map(Number)
+
+    const minutesAmount = (hours * 60) + minutes;
+
+    return minutesAmount;
+
+}
